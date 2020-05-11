2149a5a5f62d561ac156ce0f0029ec74ee74a844
José Angel Ramírez Cantú <ramirez.jose.angel@accenture.com>
2020-05-11 09:35:29 -0500

    Initial Commit

---
+++ Single App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Single App
+//
+//  Created by ramirez.jose.angel on 11/05/20.
+//  Copyright © 2020 ramirez.jose.angel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
